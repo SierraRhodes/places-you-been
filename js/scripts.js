4be11e08f7e20b379360720b85aa36ecf0a16e2d
lilbitsnpieces <alreadywrote@gmail.com>
2023-02-12 16:12:04 -0800

    Add error handling

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 // Business Logic for Visited Places 
 function Log() {
-  this.place = {};
+  this.places = {};
+  this.currentId = 0;
  }
 
+ Log.prototype.addPlaces = function(place) {
+  place.id = this.assignId();
+  this.places[place.id] = place;
+
+  if (!(place instanceof Places)) {
+    return;
+  }
+}
+
+ Log.prototype.assignId = function() {
+  this.currentId +=1
+  return this.currentId; 
+ }
 
 // Business Logic for Places
 function Places(country, state, city, year ) {
